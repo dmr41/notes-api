58fb14321325102d041ad06e539c4dce86246ed1
David Rivers <dmr41.rivers@gmail.com>
2015-01-29 14:45:15 -0700

    postgresql added sqlite3 removed

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,24 +2,24 @@ class NotesController < ApplicationController
 
   def index
     @notes = Note.all
-    render json:{notes: @notes}
+    render json: @notes
   end
 
   def show
     @note = Note.find(params[:id])
-    render json:{note: @note}
+    render json: @note
   end
 
   def update
     @note = Note.find(params[:id])
     @note.update(user_params)
-    render json:{note: @note}
+    render json: @note
   end
 
   def create
     @note = Note.new(user_params)
     @note.save
-    render json:{note: @note}
+    render json: @note
   end
 
   def destroy
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150128165859) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "notes", force: true do |t|
     t.string "body"
     t.string "title"
